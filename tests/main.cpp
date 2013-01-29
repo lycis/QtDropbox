9d00e991ef2d22aeed79e5effc8bc7bf4091d8ac
lycis <daniel@deder.at>
2013-01-30 00:52:34 +0100

    Unit tests: extended test cases for QDropboxJson
    references #12
    
    Signed-off-by: lycis <daniel@deder.at>

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,4 +1,6 @@
 #include <QTest>
+#include <QTextStream>
+
 #include "qdropboxtest.hpp"
 #include "qdropboxjsontest.hpp"
 
@@ -7,7 +9,20 @@ int main()
     QDropboxTest dropboxTest;
     QDropboxJsonTest jsonTest;
 
-    QTest::qExec(&dropboxTest);
-    QTest::qExec(&jsonTest);
+    int failed = 0;
+    failed += QTest::qExec(&dropboxTest);
+    failed += QTest::qExec(&jsonTest);
+
+    QTextStream outstr(stdout);
+
+    outstr << endl << "************************************" << endl;
+    outstr << " Test ";
+    if(failed != 0)
+        outstr << " FAILED!!!!" << endl;
+    else
+        outstr << "finished without error." << endl;
+    outstr << "************************************" << endl;
+    outstr.flush();
+
     return 0;
 }
